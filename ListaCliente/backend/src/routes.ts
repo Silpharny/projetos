2432826cf8ce55166e0ea39a134eaf987d372c72
Silpharny <silpharny@gmail.com>
2024-06-18 19:58:44 -0300

    Criando backend app

---
+++ ListaCliente/backend/src/routes.ts
@@ -0,0 +1,24 @@
+import fastify, { FastifyInstance, FastifyPluginOptions,FastifyRequest, FastifyReply } from "fastify";
+import { CreateCustomerController } from "./controllers/CreateCustomerController";
+import { ListCustomerController } from "./controllers/ListCustomersController";
+import { DeleteCustomerController } from "./controllers/DeleteCustomerController";
+
+
+export async function routes(fastfify:FastifyInstance, options:FastifyPluginOptions) {
+    
+    fastfify.get('/teste', async(request:FastifyRequest, reply:FastifyReply) => {
+        return{ok:true}
+    })
+
+    fastfify.post('/customer', async(request:FastifyRequest, reply:FastifyReply) => {
+        return new CreateCustomerController().handle(request,reply)
+    })
+
+    fastfify.get('/customers', async(request:FastifyRequest, reply:FastifyReply) => {
+        return new ListCustomerController().handle(request,reply)
+    })
+
+    fastfify.delete('/customer', async(request:FastifyRequest, reply:FastifyReply) => {
+        return new DeleteCustomerController().handle(request,reply)
+    })
+}
